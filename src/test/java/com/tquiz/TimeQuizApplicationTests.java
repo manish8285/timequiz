78af0bc081ee2a8dab5951e0dc4db60dd72ae12b
manish8285 <manishkumarsingh1997@gmail.com>
2023-05-27 13:20:01 +0530

    build version

--- src/test/java/com/tquiz/TimeQuizApplicationTests.java
+++ src/test/java/com/tquiz/TimeQuizApplicationTests.java
@@ -13,41 +13,7 @@ class TimeQuizApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
-String st="";
-		
-		
-		Date currentDate = new Date();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm");
-		
-		Date endDate=null;
-		Date startDate=null;
-		try {
-			endDate = sdf.parse("2023-05-26T18:46:00.000+00:00");
-			startDate = sdf.parse("2023-05-26T18:57:00.000+00:00");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		System.out.println("1 + = " +startDate.compareTo(currentDate));
-		System.out.println("2 - = " +endDate.compareTo(currentDate));
-		
-		if(new Date().before(startDate)) {
-			
-			st="inactive";
-			System.out.println("inactive");
-		}
-		if(new Date().after(startDate) && new Date().before(endDate)) {
-			st="active";
-			System.out.println("active");
-		}
-		if(new Date().after(endDate)){
-			System.out.println("finished");
-		}
-		
+
 	}
 
 }
